1276de1d6d7142812f739c4bdd8ada69e7c3c8af
nguyenthanhnam0212 <nguyenthanhnam0212@gmail.com>
2025-01-07 09:39:41 +0700

    .

--- Save-Res/main_cus.py
+++ Save-Res/main_cus.py
@@ -75,9 +75,8 @@ def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_
 
 @bot.on_message(filters.text)
 def save(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-	print(message.text)
 	message.delete()
-	msgid = range(4,9149)
+	msgid = range(397, 9149)
 	for i in msgid:
 		message.text = f"https://t.me/pssjs/{i}"
 		print(message.text)
