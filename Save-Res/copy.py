800d3a3f129ccdf06bd9836affa91ce8f24da4c8
nguyenthanhnam0212 <nguyenthanhnam0212@gmail.com>
2025-01-09 08:53:05 +0700

    .

--- Save-Res/copy.py
+++ Save-Res/copy.py
@@ -86,89 +86,85 @@ def save(client: pyrogram.client.Client, message: pyrogram.types.messages_and_me
 	type_media = inf.media_type.split(",")
 	from_id = inf.msgid
 	target_id = inf.target_channel
-	try:
-		for i in tqdm(range(from_id, to_id), desc="Forwarding", unit="Post"):
-			message.text = f"https://t.me/{uss}/{i}"
-			print(f"\rCompleted ID: {i} / {to_id-1}  ", end='', flush=True)
-			# joining chats
-			if "https://t.me/+" in message.text or "https://t.me/joinchat/" in message.text:
+	for i in tqdm(range(from_id, to_id), desc="Forwarding", unit="Post"):
+		message.text = f"https://t.me/{uss}/{i}"
+		print(f"\rCompleted ID: {i} / {to_id-1}  ", end='', flush=True)
+		# joining chats
+		if "https://t.me/+" in message.text or "https://t.me/joinchat/" in message.text:
 
-				if acc is None:
-					bot.send_message(message.chat.id,f"**String Session is not Set**")
+			if acc is None:
+				bot.send_message(message.chat.id,f"**String Session is not Set**")
+				return
+
+			try:
+				try: acc.join_chat(message.text)
+				except Exception as e: 
+					bot.send_message(message.chat.id,f"**Error** : __{e}__")
 					return
+				bot.send_message(message.chat.id,"**Chat Joined**")
+			except UserAlreadyParticipant:
+				bot.send_message(message.chat.id,"**Chat alredy Joined**")
+			except InviteHashExpired:
+				bot.send_message(message.chat.id,"**Invalid Link**")
 
-				try:
-					try: acc.join_chat(message.text)
-					except Exception as e: 
-						bot.send_message(message.chat.id,f"**Error** : __{e}__")
-						return
-					bot.send_message(message.chat.id,"**Chat Joined**")
-				except UserAlreadyParticipant:
-					bot.send_message(message.chat.id,"**Chat alredy Joined**")
-				except InviteHashExpired:
-					bot.send_message(message.chat.id,"**Invalid Link**")
+		# getting message
+		elif "https://t.me/" in message.text:
 
-			# getting message
-			elif "https://t.me/" in message.text:
+			datas = message.text.split("/")
+			temp = datas[-1].replace("?single","").split("-")
+			fromID = int(temp[0].strip())
+			try: toID = int(temp[1].strip())
+			except: toID = fromID
 
-				datas = message.text.split("/")
-				temp = datas[-1].replace("?single","").split("-")
-				fromID = int(temp[0].strip())
-				try: toID = int(temp[1].strip())
-				except: toID = fromID
+			for msgid in range(fromID, toID+1):
 
-				for msgid in range(fromID, toID+1):
-
-					# private
-					if "https://t.me/c/" in message.text:
-						chatid = int("-100" + datas[4])
-						
-						if acc is None:
-							bot.send_message(message.chat.id,f"**String Session is not Set**")
-							return
-						
-						handle_private(message,chatid,msgid)
-						# try: handle_private(message,chatid,msgid)
-						# except Exception as e: bot.send_message(message.chat.id,f"**Error** : __{e}__")
+				# private
+				if "https://t.me/c/" in message.text:
+					chatid = int("-100" + datas[4])
 					
-					# bot
-					elif "https://t.me/b/" in message.text:
-						username = datas[4]
-						
-						if acc is None:
-							bot.send_message(message.chat.id,f"**String Session is not Set**")
-							return
-						try: handle_private(message,username,msgid)
-						except Exception as e: bot.send_message(message.chat.id,f"**Error** : __{e}__")
+					if acc is None:
+						bot.send_message(message.chat.id,f"**String Session is not Set**")
+						return
+					
+					handle_private(message,chatid,msgid)
+					# try: handle_private(message,chatid,msgid)
+					# except Exception as e: bot.send_message(message.chat.id,f"**Error** : __{e}__")
+				
+				# bot
+				elif "https://t.me/b/" in message.text:
+					username = datas[4]
+					
+					if acc is None:
+						bot.send_message(message.chat.id,f"**String Session is not Set**")
+						return
+					try: handle_private(message,username,msgid)
+					except Exception as e: bot.send_message(message.chat.id,f"**Error** : __{e}__")
 
-					# public
-					else:
-						username = datas[3]
+				# public
+				else:
+					username = datas[3]
 
-						try: msg  = bot.get_messages(username,msgid)
-						except UsernameNotOccupied: 
-							bot.send_message(message.chat.id,f"**The username is not occupied by anyone**")
-							return
-						if get_message_type(msg) in type_media:
-							try:
-								if '?single' not in message.text:
-									bot.copy_message(target_id, msg.chat.id, msg.id)
-									# bot.copy_message(message.chat.id, msg.chat.id, msg.id)
-								else:
-									bot.copy_media_group(-1001723907536, msg.chat.id, msg.id)
-									# bot.copy_media_group(message.chat.id, msg.chat.id, msg.id)
-							except:
-								if acc is None:
-									print(f"**String Session is not Set - Skipped**")
-								# try: handle_private(message,username,msgid)
-								# except Exception as e: print(f"**Error** : __{e}__")
-							time.sleep(3)
-					links_force(username=uss, msgid=i).update_links()
-					# wait time
-					# time.sleep(3)
-	except KeyboardInterrupt:
-		print("Stopped by user")
-		return
+					try: msg  = bot.get_messages(username,msgid)
+					except UsernameNotOccupied: 
+						bot.send_message(message.chat.id,f"**The username is not occupied by anyone**")
+						return
+					if get_message_type(msg) in type_media:
+						try:
+							if '?single' not in message.text:
+								bot.copy_message(target_id, msg.chat.id, msg.id)
+								# bot.copy_message(message.chat.id, msg.chat.id, msg.id)
+							else:
+								bot.copy_media_group(-1001723907536, msg.chat.id, msg.id)
+								# bot.copy_media_group(message.chat.id, msg.chat.id, msg.id)
+						except:
+							if acc is None:
+								print(f"**String Session is not Set - Skipped**")
+							# try: handle_private(message,username,msgid)
+							# except Exception as e: print(f"**Error** : __{e}__")
+						time.sleep(3)
+				links_force(username=uss, msgid=i).update_links()
+				# wait time
+				# time.sleep(3)
 
 # handle private
 def handle_private(message: pyrogram.types.messages_and_media.message.Message, chatid: int, msgid: int):
