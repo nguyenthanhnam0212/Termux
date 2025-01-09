e55102d54de8d117118041f87e3216889038a91f
nguyenthanhnam0212 <nguyenthanhnam0212@gmail.com>
2025-01-09 08:46:59 +0700

    .

--- test.py
+++
@@ -1,8 +0,0 @@
-import time
-
-try:
-    for i in range(100):
-        print(f"Đang xử lý phần tử {i}")
-        time.sleep(1)  # Mô phỏng công việc mất thời gian
-except KeyboardInterrupt:
-    print("\nĐã dừng vòng lặp do bạn nhấn Ctrl+C!")
