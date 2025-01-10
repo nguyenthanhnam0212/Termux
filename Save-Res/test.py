3d6b1af20e0888d92766dcf80adae07650aaf7fb
nguyenthanhnam0212 <nguyenthanhnam0212@gmail.com>
2025-01-10 14:09:07 +0700

    .

--- Save-Res/test.py
+++ Save-Res/test.py
@@ -10,17 +10,17 @@ api_id = os.getenv('API_ID')
 
 user_name = 'Timiys'
 target_id = -1002333974972
-mess_id = 652
+mess_id = 677
 
 with Client("save_content_x_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token) as bot:
 	chat = bot.get_chat('Timiys')
 	try:
-		# bot.copy_message(chat_id=target_id, from_chat_id=chat.id, message_id=mess_id)
+		bot.copy_message(chat_id=target_id, from_chat_id=chat.id, message_id=mess_id)
 		print("SUCCESS")
 	except Exception as e:
 		print(f"ERROR: {e}")
 
 
-	msg = bot.get_messages('Timiys', 677)
-	print(str(msg.media).replace('MessageMediaType.','').strip())
+	# msg = bot.get_messages('Timiys', 677)
+	# print(str(msg.media).replace('MessageMediaType.','').strip())
 
