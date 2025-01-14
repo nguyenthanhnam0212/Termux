3721c8b20ade0a00270daa629ba6a572154c4af2
nguyenthanhnam0212 <nguyenthanhnam0212@gmail.com>
2025-01-14 11:43:25 +0700

    .

--- tools.py
+++ tools.py
@@ -2,6 +2,7 @@ import socket
 
 class tools:
     def show_IP():
-        hostname = socket.gethostname()
-        ip_address = socket.gethostbyname(hostname)
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            s.connect(("8.8.8.8", 80))
+            ip_address = s.getsockname()[0]
         return ip_address
