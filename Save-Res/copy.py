0dba2a7aa2f4e742a81af655d365226232b8463f
nguyenthanhnam0212 <nguyenthanhnam0212@gmail.com>
2025-01-15 08:26:09 +0700

    .

--- Save-Res/copy.py
+++ Save-Res/copy.py
@@ -33,12 +33,20 @@ with Client("save_content_x_bot", api_id=api_id, api_hash=api_hash, bot_token=bo
     print(f"From: {user_name}\nTo: {target_id}\n\nIP: {tools.show_IP()}\n\nForwarding......")
     for i in tqdm(range(int(mess_id), int(mess_end)+1), desc = "....", unit="Post"):
         msg = bot.get_messages(user_name, i)
+
+        # Lấy ra loại Media
         type_media = str(msg.media).replace('MessageMediaType.','').strip()
+
+        caption = ""
+        if type_media == 'VIDEO':
+            caption = f"{msg.caption}\n{msg.video.file_name}"
+
         if type_media in media_list:
             try:
-                bot.copy_message(chat_id=target_id, from_chat_id=chat.id, message_id=i)
+                bot.copy_message(chat_id=target_id, from_chat_id=chat.id, message_id=i, caption = caption)
                 print(f"\r{i} / {mess_end}  ", end='', flush=True)
             except Exception as e:
                 print(f"ERROR: {i}")
             links_force(username=user_name, msgid=i).update_links()
             time.sleep(3)
+
