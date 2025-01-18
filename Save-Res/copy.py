54e988601436e543e4b9c92f7144f167022c55de
nguyenthanhnam0212 <nguyenthanhnam0212@gmail.com>
2025-01-19 01:39:38 +0700

    .

--- Save-Res/copy.py
+++ Save-Res/copy.py
@@ -31,7 +31,8 @@ media_list = inf.media_type.upper().split(",")
 target_id = inf.target_channel
 
 Uncen = ["TAXD", "RKI", "259LUXU", "START", "MMUS", "MISM", "WAAA", "KKBT", "ADN", "200GANA", "NACR", "ATID", "SSIS", "IPX",
-         "MIDE", "PRED", "ABW", "SSNI", "FSDSS", "DASS", "URE", "IPZZ", "HMN", "MIAB", "MIDV", "JUQ", "SONE"]
+         "MIDE", "PRED", "ABW", "SSNI", "FSDSS", "DASS", "URE", "IPZZ", "HMN", "MIAB", "MIDV", "JUQ", "SONE", "GOOD", "HNDS",
+         "CAWD", "MEYD", "MKMP", "ROE", "HMN", "CJOD", "MFK", "SDNM", "MSAJ", "SETM", "MTABS", "HBAD"]
 
 with Client("save_content_x_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token) as bot:
     chat = bot.get_chat(user_name)
