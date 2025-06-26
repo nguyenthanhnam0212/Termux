5aa4f36131c94a6779d4a1bdd7c226059c52b5b1
nguyenthanhnam0212 <nguyenthanhnam0212@gmail.com>
2025-06-26 20:34:25 +0700

    .

--- Save-Res/test.py
+++ Save-Res/test.py
@@ -8,9 +8,9 @@ bot_token = os.getenv('TOKEN')
 api_hash = os.getenv('API_HASH')
 api_id = os.getenv('API_ID')
 
-user_name = 'laosijighs'
-target_id = -1002280926246
-mess_id = 9123
+user_name = 'xhxstory'
+target_id = -1002507681457
+mess_id = 4
 
 with Client("save_content_x_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token) as bot:
 	chat = bot.get_chat(user_name)
