e3bfbec2bc2c554bc4146099d41957cb5c451b5c
nguyenthanhnam0212 <nguyenthanhnam0212@gmail.com>
2025-09-18 22:51:14 +0700

    .

--- abyss/app.py
+++ abyss/app.py
@@ -56,14 +56,14 @@ async def upload_handler(client, message):
                 )
             ]
             await message.reply_text("⬆️ Đang upload video ...")
-            await client.send_media_group(
+            await app.send_media_group(
                 chat_id=message.chat.id,
                 media=media
             )
             _ABYSS(movie_code=movie_code, status=0).update_status()
         except:
             await message.reply_text("⬆️ Đang upload video ...")
-            await client.send_video(chat_id=message.chat.id, video=movie, caption=f"`{movie_code}`", parse_mode="Markdown")
+            await app.send_video(chat_id=message.chat.id, video=movie, caption=f"`{movie_code}`", parse_mode="Markdown")
         # os.remove(movie)
 
 @app.on_message(filters.text & ~filters.regex(r"^/"))
@@ -117,7 +117,7 @@ async def handle_download(client, message):
                 )
             ]
 
-            await client.send_media_group(
+            await app.send_media_group(
                 chat_id=message.chat.id,
                 media=media
             )
@@ -127,7 +127,7 @@ async def handle_download(client, message):
             filename_no_ext = os.path.splitext(filename)[0]
             movie_code = filename_no_ext.split("_")[0]
             await message.reply_text("⬆️ Đang upload video ...")
-            await client.send_video(
+            await app.send_video(
                 chat_id=message.chat.id,
                 video = latest_file,
                 caption=f"`{movie_code}`"
