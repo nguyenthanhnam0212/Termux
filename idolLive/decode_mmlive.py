f4abb8741a1e1207f14cc5c1a7cc23ba8cdc9767
nguyenthanhnam0212 <nguyenthanhnam0212@gmail.com>
2025-09-20 07:15:40 +0700

    .

--- idolLive/decode_mmlive.py
+++ idolLive/decode_mmlive.py
@@ -2,9 +2,9 @@ import base64
 import json
 import os
 import time
-# from Crypto.Cipher import AES
+from Crypto.Cipher import AES
 # from Crypto.Util.Padding import pad, unpad
-from Cryptodome.Cipher import AES
+# from Cryptodome.Cipher import AES
 from Cryptodome.Util.Padding import pad, unpad
 
 
