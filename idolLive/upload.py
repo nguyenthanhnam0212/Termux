163a13f1d96c7f8eadb740f533082df0251db1bb
nguyenthanhnam0212 <nguyenthanhnam0212@gmail.com>
2025-09-21 07:18:06 +0700

    .

---
+++ idolLive/upload.py
@@ -0,0 +1,57 @@
+import subprocess
+import json
+import os
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.types import InputMediaVideo, InputMediaPhoto
+
+from dotenv import load_dotenv
+load_dotenv()
+
+API_ID = os.getenv('API_ID')
+API_HASH = os.getenv('API_HASH')
+BOT_TOKEN = os.getenv('TOKEN')
+
+WORKDIR = f"/sdcard/Download/"
+
+
+def get_video_info(path: str):
+    cmd = [
+        "ffprobe", "-v", "error",
+        "-select_streams", "v:0",
+        "-show_entries", "stream=width,height,duration",
+        "-of", "json", path
+    ]
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    info = json.loads(result.stdout)
+    stream = info["streams"][0]
+    width = int(stream["width"])
+    height = int(stream["height"])
+    duration = int(float(stream.get("duration", 0)))
+    return width, height, duration
+
+app = Client("abyss_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+@app.on_message(filters.command("start"))
+async def start_handler(client, message):
+    await message.reply_text("Upload Video from Downloaded files")
+
+@app.on_message(filters.command("upload"))
+async def upload_handler(client, message):
+    files = [f for f in os.listdir(WORKDIR) if f.endswith(".mp4")]
+    if not files:
+        await message.reply_text("❌ Không có file nào trong thư mục!")
+        return
+    for file in files:
+        movie = os.path.join(WORKDIR, file)
+        filename = os.path.basename(movie)
+        filename_no_ext = os.path.splitext(filename)[0]
+        movie_code = filename_no_ext.split("_")[0]
+
+        width, height, duration = get_video_info(movie)
+
+        await message.reply_text("⬆️ Đang upload video ...")
+        await app.send_video(chat_id=message.chat.id, video=movie, width=width, height=height, duration=duration, supports_streaming=True, caption=f"`{movie_code}`", parse_mode="Markdown")
+        # os.remove(movie)
+
+app.run()
