d77b437ce89b127202ff38d30354b7803934b4dc
nguyenthanhnam0212 <nguyenthanhnam0212@gmail.com>
2025-10-01 16:02:53 +0700

    .

--- abyss/auto.py
+++ abyss/auto.py
@@ -38,9 +38,10 @@ app = Client("save_content_x_bot", api_id=API_ID, api_hash=API_HASH, bot_token=B
 async def start_handler(client, message):
     list_ID = [
         {"ID": "1X7nrBTh8", "name_en": "Home Alone"},
+        {"ID": "2n_AhbKX0", "name_en": "Home Alone 2"},
         {"ID": "2L83abiSA", "name_en": "Home Alone 3"},
-        {"ID": "1MVJSYy0C", "name_en": "Home Alone The Holiday Heist"}
-        # {"ID": "n6ylOt5EUZ", "name_en": "Mr Vampire"},
+        {"ID": "1MVJSYy0C", "name_en": "Home Alone The Holiday Heist"},
+        {"ID": "omqBuyBLr", "name_en": "Mr Vampire 3"}
         # {"ID": "vCjH_6i6x", "name_en": "Mr Vampire Saga"}
         ]
     for item in list_ID:
