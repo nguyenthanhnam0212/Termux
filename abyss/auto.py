5c7b7cc04bc79ee0dbb854f7b90b4103bfc1c619
nguyenthanhnam0212 <nguyenthanhnam0212@gmail.com>
2025-10-03 23:22:09 +0700

    .

--- abyss/auto.py
+++ abyss/auto.py
@@ -35,7 +35,7 @@ app = Client("save_content_x_bot", api_id=API_ID, api_hash=API_HASH, bot_token=B
 
 @app.on_message(filters.command("start"))
 async def start_handler(client, message):
-    list_ID = [{'ID': 'zqvwM2tTR', 'name_en': 'National Treasure: Book of Secrets'}]
+    list_ID = [{'ID': 'fBf2AYRfy', 'name_en': 'National Treasure: Book of Secrets'}]
     for item in list_ID:
         ID = item['ID']
         name_movie_en = item['name_en']
