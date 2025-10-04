020d316dfd187128c2b7c301cb62e5b6c8f539b6
nguyenthanhnam0212 <nguyenthanhnam0212@gmail.com>
2025-10-04 13:06:34 +0700

    .

--- abyss/auto.py
+++ abyss/auto.py
@@ -35,7 +35,7 @@ app = Client("save_content_x_bot", api_id=API_ID, api_hash=API_HASH, bot_token=B
 
 @app.on_message(filters.command("start"))
 async def start_handler(client, message):
-    list_ID = [{'ID': 'PnmYRavq9', 'name_en': 'Full Throttle'}]
+    list_ID = [{'ID': 'lEOarAFM0', 'name_en': '3 Idiots'}]
     for item in list_ID:
         ID = item['ID']
         name_movie_en = item['name_en']
