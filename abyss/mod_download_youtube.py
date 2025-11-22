fcd28605ecac97c3b9661a4d35a78b2219145888
nguyenthanhnam0212 <nguyenthanhnam0212@gmail.com>
2025-11-23 02:45:40 +0700

    .

--- abyss/mod_download_youtube.py
+++ abyss/mod_download_youtube.py
@@ -2,18 +2,19 @@ import yt_dlp
 from PIL import Image
 
 class Youtube:
-    def download(i, url_play):
+    def download(file_name, url_play):
         ydl_opts = {
             "format": "bv*+ba/b",
             "merge_output_format": "mp4",
             "ignoreerrors": True,
             "continue_dl": True,
-            "outtmpl": f"{i+1}.%(ext)s",
+            "outtmpl": f"{file_name}.%(ext)s",
             "writethumbnail": True,           # tải thumbnail
             "thumbnailformat": "jpg",
             "cachedir": False,              # không dùng cache
         }
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            print(f"Đang tải video {i+1} từ playlist...")
             info = ydl.extract_info(url_play, download=True)  # tải video
+            title = info['title']
+        return title
